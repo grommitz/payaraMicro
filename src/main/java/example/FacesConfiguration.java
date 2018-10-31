337f0d53f43ec9859e55a41c74a0422fadf5806d
Martin Charlesworth <martin.charlesworth@cscglobal.com>
2018-10-31 01:14:34 +0000

    working version: - put webapp folder in right place - added web.xml to configure faces servlet - changed packaging to war
    > mvn clean install
    > java -jar payara-micro-5.183.jar --port 8777 --deploy target/payare-1.0-SNAPSHOT.war
    
    http://localhost:8777/webapp/

---
+++ src/main/java/example/FacesConfiguration.java
@@ -0,0 +1,14 @@
+package example;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/**
+ * The presence of the @FacesConfig annotation on a managed bean deployed within an application enables version specific
+ * features. In this case, it enables JSF CDI injection and EL resolution using CDI.
+ *
+ */
+@ApplicationScoped
+@FacesConfig(version = FacesConfig.Version.JSF_2_3)
+public class FacesConfiguration {
+}
